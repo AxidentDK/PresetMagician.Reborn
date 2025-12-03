5f274cafdc8b901d1b76c5d111e8db3f802ab67c
AxidentDK <kimiskov@gmail.com>
2025-12-03 23:08:45 +0100

    PresetMagician reborn on .NET 8.0 x64 – clean, modern, VST3-ready – legend status achieved

---
+++ App.xaml.cs
@@ -0,0 +1,6 @@
+using System.Windows;
+
+namespace PresetMagician.Net8
+{
+    public partial class App : Application { }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,9 @@
+using System.Windows;
+
+namespace PresetMagician.Net8
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow() => InitializeComponent();
+    }
+}
